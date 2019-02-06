23d659ce66a21311ca6da5d5b8d45b5ab65dd84f
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-02-06 23:42:29 +0700

    Auth is here!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React, { useState, Suspense } from 'react'
-import { Grommet, Text, Box, Button, Heading, Paragraph, Layer } from 'grommet'
+import React, { useState, Suspense, ReactNode, useCallback } from 'react'
+import {
+  Grommet,
+  Text,
+  Box,
+  Button,
+  Heading,
+  Paragraph,
+  Layer,
+  ButtonProps
+} from 'grommet'
 import { dark, generate } from 'grommet/themes'
 import { deepMerge } from 'grommet/utils'
+import { useFirebaseAuth } from 'fiery'
+import firebase from 'firebase'
 
 const theme = deepMerge(generate(24, 6), dark, {
   global: {
@@ -17,11 +28,30 @@ function App() {
     <Grommet theme={theme} full>
       <ErrorBoundary>
         <Suspense fallback={<Loading />}>
-          <Box>
-            <Text>Text</Text>
-            <Button label="Button" onClick={() => {}} />
-            <A />
-          </Box>
+          <AuthenticationWall>
+            {user => (
+              <React.Fragment>
+                <Box pad="small" direction="row" border="bottom" align="center">
+                  <Text>
+                    Hello, <strong>{user.displayName}</strong>
+                  </Text>
+                  <Text margin={{ left: 'auto' }}>
+                    <ActionButton
+                      plain
+                      label="Sign out"
+                      color="neutral-3"
+                      onClick={async () => {
+                        if (confirm('Surely?')) await firebase.auth().signOut()
+                      }}
+                    />
+                  </Text>
+                </Box>
+                <Suspense fallback={<Loading />}>
+                  <Main user={user} />
+                </Suspense>
+              </React.Fragment>
+            )}
+          </AuthenticationWall>
         </Suspense>
       </ErrorBoundary>
     </Grommet>
@@ -30,11 +60,63 @@ function App() {
 
 function Loading() {
   return (
-    <Layer modal={false}>
+    <Layer modal={false} responsive={false}>
       <Text size="xxlarge" color="light-6">
         Loading...
       </Text>
     </Layer>
+  )
+}
+
+function Main(props: { user: firebase.User }) {
+  return <div>Hey!</div>
+}
+
+function AuthenticationWall(props: {
+  children: (user: firebase.User) => ReactNode
+}) {
+  const authState = useFirebaseAuth()
+  const me = authState.unstable_read()
+  return (
+    <React.Fragment>
+      {me ? (
+        props.children(me)
+      ) : (
+        <Box pad="medium">
+          <Heading level="1">You must sign in to continue</Heading>
+          <Paragraph>
+            <ActionButton
+              primary
+              label="Sign in with Facebook"
+              color="#365899"
+              onClick={() =>
+                firebase
+                  .auth()
+                  .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+              }
+            />
+          </Paragraph>
+        </Box>
+      )}
+    </React.Fragment>
+  )
+}
+
+function ActionButton(props: ButtonProps & JSX.IntrinsicElements['button']) {
+  const [running, setRunning] = useState(false)
+  const onClick = async (e: React.MouseEvent) => {
+    e.preventDefault()
+    setRunning(true)
+    try {
+      if (props.onClick) await props.onClick(e)
+    } catch (e) {
+      window.alert(`Error: ${e}`)
+    } finally {
+      setRunning(false)
+    }
+  }
+  return (
+    <Button {...props} onClick={onClick} disabled={props.disabled || running} />
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import firebase from 'firebase'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyDWaXE2bSrgg1AL4TG1rZmFC2NyUQ2_D2A',
+  authDomain: 'ingage-platform-demo.firebaseapp.com',
+  databaseURL: 'https://ingage-platform-demo.firebaseio.com',
+  projectId: 'ingage-platform-demo',
+  storageBucket: 'ingage-platform-demo.appspot.com',
+  messagingSenderId: '13020059910'
+}
+firebase.initializeApp(config)
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
