e20f142ccb3d00d6a0cec07581b8c1a9b91c8b9e
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-02-15 19:08:07 +0700

    Implement AudienceRoot support

--- src/core/app/AudienceRoot.tsx
+++ src/core/app/AudienceRoot.tsx
@@ -1,4 +1,45 @@
 import React from 'react'
+import firebase from 'firebase'
+import { useConfig } from './ConfigContext'
+import { useFirebaseDatabase } from 'fiery'
+import { ErrorMessage, LoadingContext } from '../ui'
+import { Box, Text } from 'grommet'
+import { SceneContext } from './SceneContext'
+
 export function AudienceRoot() {
-  return <div>Audience view</div>
+  const config = useConfig()
+  const dataRef = firebase.database().ref('/currentScene')
+  const dataState = useFirebaseDatabase(dataRef)
+  const currentScene = dataState.unstable_read()
+  const scene = config.scenes.filter(s => s.name === currentScene)[0]
+  if (!scene)
+    return (
+      <Box pad="large">
+        <Text size="xlarge" color="light-6">
+          Welcome!
+        </Text>
+      </Box>
+    )
+  const AudienceComponent = scene.audienceComponent
+  if (!AudienceComponent)
+    return (
+      <Box pad="large">
+        <Text size="xlarge" color="light-6">
+          Welcome!
+        </Text>
+      </Box>
+    )
+  const sceneContext = {
+    dataRef: firebase
+      .database()
+      .ref('/scenes')
+      .child(scene.name)
+  }
+  return (
+    <SceneContext.Provider value={sceneContext}>
+      <LoadingContext>
+        <AudienceComponent />
+      </LoadingContext>
+    </SceneContext.Provider>
+  )
 }
