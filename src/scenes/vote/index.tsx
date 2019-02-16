d79a5fdccec5d14843277d12cf1a30fa844e83b0
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-02-16 16:54:33 +0700

    Display vote leaderboard and count

--- src/scenes/vote/index.tsx
+++ src/scenes/vote/index.tsx
@@ -15,6 +15,7 @@ import λ from 'react-lambda'
 export const scene: IScene = {
   name: 'vote',
   backstageComponent: VoteBackstage,
+  presentationComponent: VotePresentation,
   audienceComponent: VoteAudience
 }
 
@@ -38,9 +39,19 @@ function VoteAudience() {
   const voteCount = firebaseToEntries(myVotes).filter(entry => entry.val).length
   const hasVotedFor = (optionId: string) => !!(myVotes && myVotes[optionId])
 
+  // max votes
   const maxVotesRef = sceneContext.dataRef.child('settings').child('maxVotes')
   const maxVotesState = useFirebaseDatabase(maxVotesRef)
   const maxVotes = maxVotesState.unstable_read() || DEFAULT_MAX_VOTES
+
+  // enabled?
+  const enabledRef = sceneContext.dataRef.child('settings').child('enabled')
+  const enabledState = useFirebaseDatabase(enabledRef)
+  const enabled = enabledState.unstable_read()
+
+  if (!enabled) {
+    return <Box pad="medium">Wait for voting to open...</Box>
+  }
 
   return (
     <Box pad="small">
@@ -84,10 +95,6 @@ function VoteBackstage() {
   const textValue =
     voteOptionsText != null ? voteOptionsText : initialVoteOptionsText
 
-  const maxVotesRef = sceneContext.dataRef.child('settings').child('maxVotes')
-  const maxVotesState = useFirebaseDatabase(maxVotesRef)
-  const maxVotes = maxVotesState.unstable_read() || DEFAULT_MAX_VOTES
-
   return (
     <Box>
       <BackstageSection title="Available options">
@@ -115,23 +122,71 @@ function VoteBackstage() {
               successMessage="Vote options has been set"
             />
           </Box>
+          <Box flex={false} margin={{ right: 'small' }}>
+            {λ(() => {
+              const maxVotesRef = sceneContext.dataRef
+                .child('settings')
+                .child('maxVotes')
+              const maxVotesState = useFirebaseDatabase(maxVotesRef)
+              const maxVotes =
+                maxVotesState.unstable_read() || DEFAULT_MAX_VOTES
+              return (
+                <ActionButton
+                  label={`Max votes: ${maxVotes}`}
+                  description="set max votes"
+                  onClick={async () => {
+                    const howMany = +prompt(
+                      'How many votes max per person?',
+                      maxVotes
+                    )!
+                    if (howMany) await maxVotesRef.set(howMany)
+                  }}
+                  successMessage="Maximum vote count has been set"
+                />
+              )
+            })}
+          </Box>
           <Box flex={false}>
-            <ActionButton
-              label={`Max votes: ${maxVotes}`}
-              description="set max votes"
-              onClick={async () => {
-                const howMany = +prompt(
-                  'How many votes max per person?',
-                  maxVotes
-                )!
-                if (howMany) await maxVotesRef.set(howMany)
-              }}
-              successMessage="Maximum vote count has been set"
-            />
+            {λ(() => {
+              const enabledRef = sceneContext.dataRef
+                .child('settings')
+                .child('enabled')
+              const enabledState = useFirebaseDatabase(enabledRef)
+              const enabled = enabledState.unstable_read()
+              return (
+                <ActionCheckbox
+                  checked={enabled}
+                  toggle
+                  label={`Enabled`}
+                  description="toggle voting"
+                  onChange={async () => {
+                    await enabledRef.set(!enabled)
+                  }}
+                />
+              )
+            })}
           </Box>
         </Box>
       </BackstageSection>
       <BackstageSection title="Vote results">
+        {λ(() => {
+          const showResultsRef = sceneContext.dataRef
+            .child('settings')
+            .child('showResults')
+          const showResultsState = useFirebaseDatabase(showResultsRef)
+          const showResults = showResultsState.unstable_read()
+          return (
+            <ActionCheckbox
+              checked={showResults}
+              toggle
+              label={`Show results`}
+              description="toggle result visibility"
+              onChange={async () => {
+                await showResultsRef.set(!showResults)
+              }}
+            />
+          )
+        })}
         {λ(() => {
           const allVotesRef = sceneContext.dataRef.child('votes')
           const allVotesState = useFirebaseDatabase(allVotesRef)
@@ -158,6 +213,93 @@ function VoteBackstage() {
           )
         })}
       </BackstageSection>
+    </Box>
+  )
+}
+
+function VotePresentation() {
+  const sceneContext = useSceneContext()
+  const [voteOptionsText, setVoteOptionsText] = useState<string | null>(null)
+
+  const optionsRef = sceneContext.dataRef.child('options')
+  const optionsState = useFirebaseDatabase(optionsRef)
+  const options = optionsState.unstable_read()
+
+  const showResultsRef = sceneContext.dataRef
+    .child('settings')
+    .child('showResults')
+  const showResultsState = useFirebaseDatabase(showResultsRef)
+  const showResults = showResultsState.unstable_read()
+
+  if (!showResults) {
+    return (
+      <Box fill align="center" justify="center">
+        <Text alignSelf="center" textAlign="center" size="64px">
+          {λ(() => {
+            const allVotesRef = sceneContext.dataRef.child('votes')
+            const allVotesState = useFirebaseDatabase(allVotesRef)
+            const votesPerUser = firebaseToEntries(
+              allVotesState.unstable_read()
+            ).map(
+              entry =>
+                firebaseToEntries(entry.val).filter(voteEntry => voteEntry.val)
+                  .length
+            )
+            const numberOfPeople = votesPerUser.length
+            const numberOfVotes = votesPerUser.reduce((a, b) => a + b, 0)
+            return (
+              <span>
+                {numberOfVotes} vote{numberOfVotes === 1 ? '' : 's'} from{' '}
+                {numberOfPeople} {numberOfPeople === 1 ? 'person' : 'people'}
+              </span>
+            )
+          })}
+        </Text>
+      </Box>
+    )
+  }
+
+  return (
+    <Box fill>
+      {λ(() => {
+        const allVotesRef = sceneContext.dataRef.child('votes')
+        const allVotesState = useFirebaseDatabase(allVotesRef)
+        const allVotes = firebaseToEntries(allVotesState.unstable_read())
+        const voteResult = firebaseToEntries(options)
+          .map(entry => {
+            const optionId = entry.key
+            const optionText = entry.val
+            const voteCount = allVotes.filter(voterEntry => {
+              const voterVotes = voterEntry.val
+              return !!(voterVotes && voterVotes[optionId])
+            }).length
+            return { optionText, voteCount }
+          })
+          .sort((a, b) => b.voteCount - a.voteCount)
+        return (
+          <Box fill>
+            <Box pad="medium" background="dark-1" flex={false}>
+              <Text alignSelf="center" textAlign="center" size="64px">
+                Voting Results
+              </Text>
+            </Box>
+            <Box
+              flex
+              pad="small"
+              style={{ fontSize: '56px', lineHeight: '72px' }}
+            >
+              {voteResult.map(entry => {
+                return (
+                  <Box direction="row" pad="small">
+                    <Box flex>{entry.optionText}</Box>
+                    <Box style={{ textAlign: 'right' }}>{entry.voteCount}</Box>
+                  </Box>
+                )
+              })}
+            </Box>
+          </Box>
+        )
+      })}
     </Box>
   )
 }
