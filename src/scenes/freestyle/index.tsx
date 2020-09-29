89863ca72fc50a7e01d7ad16ee9eb5be996fb157
dtinth on Cloud9 <dtinth@spacet.me>
2020-09-29 18:25:56 +0000

    Custom audience text

--- src/scenes/freestyle/index.tsx
+++ src/scenes/freestyle/index.tsx
@@ -1,19 +1,8 @@
-import { useFirebaseAuth, useFirebaseDatabase } from 'fiery'
-import { Box, DataTable, Text, TextInput } from 'grommet'
-import React, { ChangeEvent, useState } from 'react'
-import { firebaseToEntries } from '../../core/app'
-import {
-  SceneDataConnector,
-  useSceneContext,
-} from '../../core/app/SceneContext'
+import { Box, TextArea } from 'grommet'
+import React, { ReactNode, useState } from 'react'
+import { SceneDataConnector } from '../../core/app/SceneContext'
 import { IScene } from '../../core/model'
-import {
-  ActionButton,
-  ActionCheckbox,
-  flashError,
-  InlineLoadingContext,
-  Panel,
-} from '../../core/ui'
+import { ActionButton, Draft, Field, Panel } from '../../core/ui'
 
 export const scene: IScene = {
   name: 'freestyle',
@@ -23,14 +12,47 @@ export const scene: IScene = {
 }
 
 function FreestyleAudience() {
-  return <Box pad="small">Freestyle!</Box>
+  return (
+    <Box pad="small">
+      <SceneDataConnector
+        path={['main', 'settings', 'public-read', 'audienceText']}
+      >
+        {(audienceText) => (
+          <div
+            dangerouslySetInnerHTML={{ __html: String(audienceText.value) }}
+          />
+        )}
+      </SceneDataConnector>
+    </Box>
+  )
 }
 
 function FreestyleBackstage() {
   return (
     <Box gap="medium">
-      <Panel title="Example">
-        <Box pad="small">Freestyle!</Box>
+      <Panel title="Audience view">
+        <Box pad="small">
+          <Field label="Text to show">
+            <SceneDataConnector
+              path={['main', 'settings', 'public-read', 'audienceText']}
+            >
+              {(audienceText) => (
+                <Draft
+                  value={audienceText.value || ''}
+                  onSave={(value) => audienceText.ref.set(value)}
+                >
+                  {(draft, setDraft) => (
+                    <TextArea
+                      rows={8}
+                      value={draft}
+                      onChange={(e) => setDraft(e.target.value)}
+                    />
+                  )}
+                </Draft>
+              )}
+            </SceneDataConnector>
+          </Field>
+        </Box>
       </Panel>
     </Box>
   )
