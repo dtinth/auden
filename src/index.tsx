eb319b44bbce2870af78b74a6191185d04caf003
Thai Pangsakulyanont <dtinth@spacet.me>
2024-05-16 16:27:38 +0700

    Migrate codebase to Vite

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,8 @@ import { AppContainer } from './AppContainer'
 import { initializeApp } from './AppInitializer'
 import { checkHashForEventpopTicketToken } from './core/app/EventpopIntegration'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
 
 initializeApp()
 checkHashForEventpopTicketToken()
 
-ReactDOM.render(<AppContainer />, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+ReactDOM.createRoot(document.getElementById('root')).render(<AppContainer />)
